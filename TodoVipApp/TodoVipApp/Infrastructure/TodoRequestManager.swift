ed697006cbd0a5ee1b52cad64202dc27b59c9bbd
Taeangel <wngks2050@gmail.com>
2023-03-27 00:58:37 +0900

    feat: TodoRequestManager 생성

--- TodoVipApp/TodoVipApp/Infrastructure/TodoRequestManager.swift
+++ TodoVipApp/TodoVipApp/Infrastructure/TodoRequestManager.swift
@@ -8,11 +8,75 @@
 import Foundation
 
 enum TodoRequestManager {
-  case getAllTodo
+  case getTodos(page: Int, orderBy: String, perPage: Int)
   
   private var baseURL: String {
     return "https://phplaravel-574671-2962113.cloudwaysapps.com/api/v1/"
   }
   
+  private var endPoint: String {
+    
+    switch self {
+    case .getTodos:
+      return "todos?"
+    }
+  }
   
+  private var method: HTTPMethod {
+    switch self {
+    case .getTodos:
+      return .post
+    }
+  }
+  
+  private var parameters: [String: Any]? {
+    switch self {
+    case let .getTodos(page, orderBy, perPage):
+      var params: [String: Any] = [:]
+      params["page"] = page
+      params["order_by"] = orderBy
+      params["per_page"] = perPage
+      return params
+    }
+  }
+  
+  private var headerFields: [String: String]? {
+    switch self {
+    case .getTodos:
+      return ["Content-Type": "application/json"]
+    }
+  }
+  
+  private var bodyData: Data? {
+    switch self {
+    case .getTodos:
+      return nil
+    }
+  }
+  
+  var urlRequest: URLRequest {
+    var components = URLComponents(string: baseURL + endPoint)
+    
+    if let parameters {
+      components?.queryItems = parameters.map { key, value in
+        URLQueryItem(name: key, value: "\(value)")
+      }
+    }
+    
+    var request = URLRequest(url: (components?.url) ?? URL(fileURLWithPath: ""))
+    request.httpMethod = method.rawValue
+    
+    if let headerFields {
+      headerFields.forEach {
+        request.addValue($0.value, forHTTPHeaderField: $0.key)
+      }
+    }
+    
+    if let bodyData {
+      request.httpBody = bodyData
+    }
+    
+    return request
+  }
 }
+
