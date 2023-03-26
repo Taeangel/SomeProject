45c315bb481f095ead5500b9ac200e069c107da4
Taeangel <wngks2050@gmail.com>
2023-03-27 00:58:14 +0900

    feat: HTTPMethod 생성

---
+++ TodoVipApp/TodoVipApp/Infrastructure/HTTPMethod.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPMethod.swift
+//  TodoVipApp
+//
+//  Created by song on 2023/03/27.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+  case get = "GET"
+  case post = "POST"
+  case delete = "DELETE"
+  case patch = "PATCH"
+}
