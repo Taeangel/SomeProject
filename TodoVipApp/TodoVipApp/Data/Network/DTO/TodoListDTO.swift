3d678ab5d1fd676a29f79b17203bbc734b838750
Taeangel <wngks2050@gmail.com>
2023-03-28 23:40:41 +0900

    feat: TodoListDTO 생성

---
+++ TodoVipApp/TodoVipApp/Data/Network/DTO/TodoListDTO.swift
@@ -0,0 +1,45 @@
+//
+//  TodoListDTO.swift
+//  TodoVipApp
+//
+//  Created by song on 2023/03/28.
+//
+
+import Foundation
+
+// MARK: - Todo
+struct TodoListDTO: Codable {
+  let data: [Datum]?
+  let meta: Meta?
+  let message: String?
+}
+
+// MARK: - Datum
+struct Datum: Codable {
+  let id: Int?
+  let title: String?
+  let isDone: Bool?
+  let createdAt, updatedAt: String?
+  
+  enum CodingKeys: String, CodingKey {
+    case id, title
+    case isDone = "is_done"
+    case createdAt = "created_at"
+    case updatedAt = "updated_at"
+  }
+}
+
+// MARK: - Meta
+struct Meta: Codable {
+  let currentPage, from, lastPage, perPage: Int?
+  let to, total: Int?
+  
+  enum CodingKeys: String, CodingKey {
+    case currentPage = "current_page"
+    case from
+    case lastPage = "last_page"
+    case perPage = "per_page"
+    case to, total
+  }
+}
+
