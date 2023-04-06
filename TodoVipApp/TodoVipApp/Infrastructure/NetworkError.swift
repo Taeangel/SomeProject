62a594e640dcd337e3f2034b68bc3450c7073399
Taeangel <wngks2050@gmail.com>
2023-04-06 22:35:09 +0900

    refactor: NetworkError 수정

--- TodoVipApp/TodoVipApp/Infrastructure/NetworkError.swift
+++ TodoVipApp/TodoVipApp/Infrastructure/NetworkError.swift
@@ -8,10 +8,24 @@
 import Foundation
 
 enum NetworkError: Error {
-  case network(error: Error)
   case decoding
-  case unknown
-  case unauthorized
   case noContent
-  case badStatus(code: URLResponse)
+  case badRequest
+  case developerProblem
+  case badStatus(code: Int)
+}
+
+extension NetworkError: LocalizedError {
+  var errorDescription: String? {
+    switch self {
+    case let .badStatus(error):
+      return "상태코드에러입니다.\(error)"
+    case .badRequest:
+      return "잘못된 정보를 입력하셨습니다. 할일은 6글자 이상 입력해 주세요"
+    case .developerProblem:
+      return "개발자에게 문의해 주십시오"
+    default:
+      return ":"
+    }
+  }
 }
